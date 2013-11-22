6e9ac4d89bb9ab87c29273db2bf910c80f500731
menginar <m.enginars@gmail.com>
2013-11-22 19:15:42 +0200

    soso

---
+++ ozgecmis.php
@@ -0,0 +1,51 @@
+<?php
+
+include '_header.php';
+
+?>
+
+ <div class="container">
+      <div class="row">
+        <div class="span8">
+           <div class="well">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h2 id="type-blockquotes" style ="margin-top: 60px;"><font color = "#800517"><center><h3> Özgeçmiş Nedir ? </h3></center></font></h2>
+                </div>
+              </div>
+                   <p>Potansiyel işverene kısaca kendinizi sunacağınız bir kısa tarihçenizdir. Özgeçmişin amacı sizi iş görüşmesine ulaştırmaktır.  
+                        CV'.'nin açılımı; Latince Curriculum Vitae'.' dir. Curriculum; Rota, yol, Vitae; Hayat demektir. Fransızca Resume; 
+                        özet anlamına gelir. 
+                   <p>Hedefe uygun bir CV yararlı, kolaylaştırıcı olabilirken, kötü bir CV olası fırsatları bir anda yok edebilmektedir.
+
+                    <p>Bu yüzden CV hazırlamak, sunmak çok önemlidir. Genel olarak yazılı metin şeklinde olmakla beraber, son yıllarda 
+                        başvurulacak işlerin niteliklerine göre, yaratıcı, farklı, çeşitli formattaki CV uygulamaları da kullanılmakta, kabul 
+                        görmekte, beğenilmekte hatta işverenler talep edilmektedir. 
+            </div>
+          </div>
+        </div>
+      </div>
+
+ <div class="container">
+      <div class="row">
+        <div class="span8">
+           <div class="well">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h2 id="type-blockquotes" style ="margin-top: 60px;"><font color = "#800517"><center><h3> Yanılgılar  </h3></center></font></h2>
+                </div>
+              </div>
+                <p><font color = "#800517"> 1. </font>  CV’lerde aranan fazla işyeri tecrübesi değildir. Aksine sık iş değiştirmiş adaylar, olumsuz değerlendirilir.
+                <p><font color = "#800517"> 2. </font> CV hayat hikayenizi anlatacağınız bir format değildir.
+                <p><font color = "#800517"> 3. </font>  Başvurduğunuz şirkette çalışmayı çok istiyor ve hangi görev olursa olsun kabul etmeyi düşünüyor olsanız da, bu durumu karşı tarafa CV veya ön yazınızla yansıtmayın.
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php
+       
+include '_footer.php';
+
+?>
