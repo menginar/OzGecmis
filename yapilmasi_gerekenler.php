5eff4a8b65584b20c7a54c34ea4694c76b101c4c
menginar <m.enginars@gmail.com>
2013-11-22 19:18:03 +0200

    soso

---
+++ yapilmasi_gerekenler.php
@@ -0,0 +1,35 @@
+<?php
+
+include '_header.php';
+
+?>
+
+   <div class="container">
+      <div class="row">
+        <div class="span8">
+           <div class="well">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h2 id="type-blockquotes" style ="margin-top: 60px;"><font color = "#800517"><center><h3> Yapılması Gerekenler</h3></center></font></h2>
+                </div>
+              </div>
+               <p><font color = "#800517"> 1. </font>Hangi pozisyon için başvuruda bulunuyor olursanız olun, CV’de yazım ve noktalama işaretlerine dikkat edin. Bu konuda iyi değilseniz bir arkadaşınızdan yardım rica edin.
+               <p><font color = "#800517"> 2. </font>İşinize hakim olduğunuzu CV’nizi değerlendirecek olan kişilere gösterebilmek için işinizle ilgili anahtar kelimeleri kullanın.
+               <p><font color = "#800517"> 3. </font>CV’nizi Word formatında hazırladıysanız gönderirken mutlaka PDF formatına çevirin.
+               <p><font color = "#800517"> 4. </font><font>CV dosyanızın adını “Ad Soyad” şeklinde gönderin. “yeni CV”, “özgeçmiş son” “_CV_revize” gibi isimler ile göndermeyin.
+               <p><font color = "#800517"> 5. </font><font>E-mail ile başvuruda bulunuyorsanız, e-mailin başlık kısmına “Başvurduğunuz Pozisyon – Ad Soyad” yazıp yollayın. Genel bir başvuruda bulunuyorsanız “Genel Başvuru – Ad Soyad” yazın.
+               <p><font color = "#800517"> 6. </font><font>Mailinizin ulaşıp ulaşmadığına dair teyit almak istediğinizi kibarca yazın, ama ilgili kişiyi sıkıştırmayın.
+               <p><font color = "#800517"> 7. </font><font>Anlayışlı bir ekip arkadaşı olacağınızı göstermek için, mailinizi yolladığınız kişiye, yazınızın sonunda özgeçmişinizi okumaya zaman ayırdığı için teşekkür edin.
+               <p><font color = "#800517"> 8. </font><font>CV’nizde veya ön yazınızda şirketin ve işin size katacaklarını bir veya iki cümleyle aktarmaya çalışın.
+               <p><font color = "#800517"> 9. </font><font>CV’nizi hazırladıktan sonra mutlaka kontrol edin. Sizin atlamış olacağınız hataları yakalayabilmek veya başka bir görüş alabilmek için bir arkadaşınızdan veya İK uzmanından yardım rica edin.
+               <p><font color = "#800517"> 10. </font><font>Yapt1ığınız başvuruyu mailinizde saklayın.
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php
+
+include '_footer.php';
+
+?>
