a80a69b8a0867b1312c6721835df74105849f2b9
menginar <m.enginars@gmail.com>
2013-11-22 19:15:11 +0200

    soso

---
+++ oneri.php
@@ -0,0 +1,40 @@
+<?php
+    
+include '_header.php';
+
+?>
+
+<div class="container">
+      <div class="row">
+        <div class="span8">
+           <div class="well">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h2 id="type-blockquotes" style ="margin-top: 60px;"><font color = "#800517"><center><h3> Öneriler </h3></center></font></h2>
+                </div>
+              </div>
+                  <p><font color = "#800517"> 1. </font>CV’nizin 1 sayfa olmasına özen gösterin.  Fakat sadece 1 sayfa olması için yazıları sıkıştırıp okunmasını zor hale getirmeyin.
+                  <p><font color = "#800517"> 2. </font>CV’nizin kolay okunur olmasına dikkat edin. Yazı fontunu 11 veya 12 olarak belirleyin.
+                  <p><font color = "#800517"> 3. </font>Farklılaşmak her zaman iyidir. Özellikle yaratıcılık gerektiren pozisyonlara başvuruyorsanız alışılmış formatların dışına çıkmakta özgür olduğunuzu bilin.
+                  <p><font color = "#800517"> 4. </font>İşyeri adının yanına şirketin logosunu da ekleyin. Böylece görsel hafızası, sözel hafızasından kuvvetli olan kişilerin CV’nizi olumlu değerlendirmesini sağlayın.
+                  <p><font color = "#800517"> 5. </font>Sizi yansıttığını düşündüğünüz fotoğrafınızı CV’nize ekleyin.
+                  <p><font color = "#800517"> 6. </font>Referanslar bölümünü doldurmayın. “İstenildiğinde paylaşılacaktır.” gibi bir cümle kullanın.
+                  <p><font color = "#800517"> 7. </font>Özellikle istediğiniz bir şirket varsa iş ilanını dikkatlice okuyup o işe uygun CV hazırlayın. İş ilanında yer alan özelliklere sahip olduğunuzu karşı tarafa gösterebilecek bilgilerinizi ekleyin.   
+                  <p><font color = "#800517"> 8. </font>Tekrarlardan kaçının. Eş anlamlı cümleler kullanmayın.
+                  <p><font color = "#800517"> 9. </font>Abartılı cümlelerden kaçının.
+                  <p><font color = "#800517"> 10. </font>CV’de, ön yazıda veya yolladığınız e-mailde şirket ve iş ile ilgili heyecanınızı aktarmaya çalışın.
+
+        
+
+            </div>
+          </div>
+        </div>
+      </div>
+<?php
+    
+include '_footer.php';
+
+?>
+
+
+            
