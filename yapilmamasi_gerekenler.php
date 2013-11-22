4e4c2963486f804064dd141d640e70ada434a988
menginar <m.enginars@gmail.com>
2013-11-22 19:16:09 +0200

    soso

---
+++ yapilmamasi_gerekenler.php
@@ -0,0 +1,39 @@
+<?php
+
+include '_header.php';
+
+?>
+   <div class="container">
+      <div class="row">
+        <div class="span8">
+           <div class="well">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h2 id="type-blockquotes" style ="margin-top: 60px;"><font color = "#800517"><center><h3> Yapılmaması Gerekenler </h3></center></font></h2>
+                </div>
+              </div>
+                <p><font color = "#800517">  1. </font>İş yeri bilgilerinizi yazarken şirketin resmi ünvanını yazmayın.
+                    Şirketinizin, diğer insanlar tarafından bilinen marka adını yazın. Ltd, A.Ş. gibi gereksiz ayrıntıları eklemeyin.
+                <p><font color = "#800517">  2. </font>İş tanımınızı paragraf halinde yazmayın. Maddeler halinde hazırlayın.
+                <p><font color = "#800517">  3. </font>CV’nize neden işten ayrıldığınız gibi negatif cümleler veya anlamlar içeren cümleler yazmayın.
+                <p><font color = "#800517">  4. </font>CV’nize gerçek olmayan bilgileri dahil etmeyin.
+                <p><font color = "#800517">  5. </font>CV’nizi okuyacak kişinin dikkatini dağıtacağından gereğinden fazla koyu (bold), yatık (italik) ve alt çizgili yazı formatlarını kullanmayın.
+                <p><font color = "#800517">  6. </font>Sırf CV’nizi doldurmak için gereksiz bilgiler eklemeyin.
+                <p><font color = "#800517">  7. </font>CV’nizi el yazısı ile hazırlamayın.
+                <p><font color = "#800517">  8. </font>Bir mail içerisinde birden fazla şirkete başvuruda bulunmak, mailiniz şirkete ulaştığı anda değerlendirme dışı tutulmanıza neden olur. 
+                     Bu yüzden bir maile birden fazla şirketi koyup başvuruda bulunmayın.
+                <p><font color = "#800517">  9. </font>CV’ye yazdığınız mail adresiniz sizi yansıtır. 
+                            Mümkün olduğunca ad soyad kombinasyonlu mail adresleri kullanın. 
+                            Çizgi film kahramanı veya takma adınız olan bir mail adresiniz varsa iş başvurularında kullanabilmek için başka bir mail adresi alın.
+                <p><font color = "#800517">  10. </font>CV’nizi içeren maili boş olarak göndermeyin. Kısa ve mümkün olduğunca samimi bir içerik hazırlayın.
+            </div>
+          </div>
+        </div>
+      </div>
+
+   
+<?php
+       
+include '_footer.php';
+
+?>          
